55b108d11b7c2ad6269a2599a63ba590159546da
kerenKi <kkinberg13@gmail.com>
2019-05-17 10:58:42 +0200

    Add correct answer out of total

--- src/actions/DisplayUserScore.js
+++ src/actions/DisplayUserScore.js
@@ -2,7 +2,6 @@
 export const DISPLAY_USERSCORE = "DISPLAY_USERSCORE";
 
 export function DisplayUserScore(score) {
-  console.log('score'+score);
   return {
     type: DISPLAY_USERSCORE ,
     payload: score
--- src/components/game-one/Score.js
+++ src/components/game-one/Score.js
@@ -11,42 +11,52 @@ import { Progress } from 'reactstrap';
 import "../../style/Progress.css";
 
 class Score extends Component {
-  correct = 0;
-  total = 0;
-//shows user score in percentage
-  getScoreInPercentage = () => {
-    let score=0;
-    this.total = this.props.scores.length;
-    this.correct = this.props.scores.filter((score) => {
-      return score === true;
-    }).length;
-    if (this.total > 0) {
-      score=Number(((this.correct / this.total) * 100).toFixed())
-      this.props.DisplayUserScore({score:score});
-      return score;
+    correct = 0;
+    total = 0;
+
+    //shows the number of correct answers
+    findOutSCore = () => {
+        this.total = this.props.scores.length;
+        this.correct = this.props.scores.filter((score) => {
+            return score === true;
+        }).length; 
+        return this.correct + "/" + this.total 
     }
-    return 0
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="text-center">Score</div>
-        {/* progreebar shows the value in percentage */}
-        <Progress className="progress" animated='true' color="info" value={ this.getScoreInPercentage()} > {this.getScoreInPercentage()}%</Progress>
-      </div>
-    )
-  }
-}
-
-
-// Redux: get both part of states
-const mapStateToProps = state => {
-  console.log(state)
-  return {
-    scores: state.gamescore
-  };
-};
-
-// Redux: connect to state, bind action creator
-export default connect(mapStateToProps,{DisplayUserScore})(Score);
+    
+    //shows user score in percentage  
+    getScoreInPercentage = () => {
+        let score=0;
+        this.total = this.props.scores.length;
+        this.correct = this.props.scores.filter((score) => {
+            return score === true;
+        }).length;
+        if (this.total > 0) {
+            score=Number(((this.correct / this.total) * 100).toFixed())
+            this.props.DisplayUserScore({score:score});
+            return score;
+        }
+        return 0
+    }
+    
+    render() {
+        return (
+            <div>
+            <div className="text-center">Score</div>
+            {/* progreebar shows the value in percentage */}
+            <Progress className="progress" animated='true' color="info" value={ this.getScoreInPercentage()} > {this.getScoreInPercentage()}%</Progress>
+            <span className="out-of-score" > {this.findOutSCore()} </span>
+            </div>
+            )
+        }
+    }
+    
+    
+    // Redux: get both part of states
+    const mapStateToProps = state => {
+        return {
+            scores: state.gamescore
+        };
+    };
+    
+    // Redux: connect to state, bind action creator
+    export default connect(mapStateToProps,{DisplayUserScore})(Score);
